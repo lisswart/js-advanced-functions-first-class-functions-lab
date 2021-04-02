2ab1a7c4886bc7f7ccca7e610bdb232b9ebc9d23
lisswart <arnoudlisa@gmail.com>
2021-04-02 08:04:44 -0400

    commit tree ba85a90e33d1724d6d9d20d0cffec9a083a7aaf3

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function(drivers) {
-    const firstTwoDrivers = [drivers[0], drivers[1]];
-    return firstTwoDrivers;
+    return drivers.slice(0, 2);
 }
 
 const returnLastTwoDrivers = function(drivers) {
-    const lastTwoDrivers = [drivers[(drivers.length)-2], drivers[(drivers.length)-1]];
-    return lastTwoDrivers;
+    return drivers.slice(-2);    
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
@@ -22,10 +20,5 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 const selectDifferentDrivers = function(arrayOfDrivers, fn) { 
-    if(fn === returnFirstTwoDrivers) {
-        return returnFirstTwoDrivers(arrayOfDrivers);
-    }
-    else if(fn === returnLastTwoDrivers) {
-        return returnLastTwoDrivers(arrayOfDrivers);
-    }
+    return fn(arrayOfDrivers);
 }
